06ef7bd62339d8bed0485512ffefbb97996e6b20
Zhadow12 <nery_ceb14@hotmail.com>
2016-11-16 12:01:33 -0700

    debo editar el botoncito con texto e imagenes

--- js/eventos.js
+++ js/eventos.js
@@ -1,13 +1,26 @@
 
 //DOM= Modelo de objetos del documento
-var inicio = function() //Main
 
-	var dameclic = function()
-{
-	alert("Le di clic a un botón");
+var inicio=function() {
+
+
+	var dameclic=function(){
+	/*alert("Le di clic a un botón");*/
+
+	$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+  	$("#txtNombre").val(data.results[0].name.first + " "+ data.results[0].name.last);
+  	$("imgFoto").attr("src",data.results[0].picture.large);
+
+    console.log(data.results[0].name.first + " "+ data.results[0].name.last);
+  }
+});
+      
 }
 	$("#dameClic").on("click",dameclic);
-
+}
 
 
 //$("elemento").off();
