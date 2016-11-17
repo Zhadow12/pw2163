82e7fff2615ef82155ae750bd6ff76d53f5f8a8c
Zhadow12 <nery_ceb14@hotmail.com>
2016-11-17 11:52:30 -0700

    ya salio casi las cosas del js yay

--- js/eventos.js
+++ js/eventos.js
@@ -11,8 +11,11 @@ var inicio=function() {
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data) {
+    $("#imgFoto").show("slow");
+    $("#txtNombre").show("slow");
   	$("#txtNombre").val(data.results[0].name.first + " "+ data.results[0].name.last);
-  	$("imgFoto").attr("src",data.results[0].picture.large);
+  	$("#imgFoto").attr("src",data.results[0].picture.large);
+    $("#miArticle").html("Texto");
 
     console.log(data.results[0].name.first + " "+ data.results[0].name.last);
   }
