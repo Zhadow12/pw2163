5defe9116391dff93b1a02b1ae0163b843be72a2
Zhadow12 <nery_ceb14@hotmail.com>
2016-11-24 12:01:10 -0700

    al fin un commit

--- ejemplo.php
+++ ejemplo.php
@@ -1,16 +1,45 @@
+
+
 <h1>Alta de usuarios</h1>
-<form action= "ejemplos.php" methos="POST">
+<form action= "registro_ejemplo.php" method="POST">
 	<input type="hidden" name="txtOculto" value="guardaUsuario">
-	<input type="text" name="txtUsuario" id="txtUsuario">
-	<input type="text" name="txtNombre" id="txtNombre">
-	<input type="password" name="txtClave" id="txtClave">
-	<select name="txtTipo" id="txtTipo">
+	<input type="text" name="txtUsuario" id="txtUsuario"><br>
+	<input type="text" name="txtNombre" id="txtNombre"><br>
+	<input type="password" name="txtClave" id="txtClave"><br>
+	<select name="txtTipo" id="txtTipo"><br>
 		<option value="administrador"> Administrador</option>		
 		<option value="invitado"> Invitado</option>
 		<option value="colado"> Colado</option>
 	</select><br>
-	<input type "submit" value="Enviar">
+	<input type="submit" value="Enviar">
 </form>
-<?php
+<hr>
 
+<?php
+//Conecto al servidor
+$conexión = mysql_connect("localhost", "root", "");
+mysql_select_db("bd2163");
+$consulta= "select * from usuarios order by usuario";
+$resultado= mysql_query($consulta);//Ejecutando consultas
+$tabla = "<table border=1>";
+$tabla.="<tr>";
+$tabla.="<th>Usuario</th><th>Nombre</th><th>Clave</th><th>Tipo</th> ";
+$tabla.="</tr>";
+
+
+while ($registro = mysql_fetch_array($resultado))
+{
+$tabla.="<tr>";
+$tabla.="<td>".$registro["usuario"]."</td>";
+$tabla.="<td>".$registro["nombre"]."</td>";
+$tabla.="<td>".$registro["clave"]."</td>";
+$tabla.="<td>".$registro["tipousuario"]."</td>";
+$tabla.="</tr>";
+
+
+
+}
+
+$tabla.="</table>";
+print $tabla;
 ?>
