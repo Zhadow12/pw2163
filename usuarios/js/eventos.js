dc05d9d50d58a87d8401ec5cff02bac4df4b1b6a
Zhadow12 <nery_ceb14@hotmail.com>
2016-11-30 11:54:54 -0700

    Esto va a empeorar antes que mejorar

--- usuarios/js/eventos.js
+++ usuarios/js/eventos.js
@@ -1,41 +1,60 @@
+
+
 var inicioUsuarios = function()
 {
 	
-	var validaUsuario = function(){
+	var validaUsuario = function()
+	{
 
-		var usuario= $("txtUsuario").val();
-		var clave = $("txtClave").val();
+		//Extraer los datos de los input en el HTML
+		var usuario = $("#txtUsuario").val();
+		var clave   = $("#txtClave").val();
 		//Preparar los parámetros para AJAX
-		var parametros= "opcion=valida"+"&usuario="usuario+"&clave"+clave+"&id"+Math.random();
-
-		//hacemos la petición remota
-		$.ajax({
-			cache:false,
-			type:"POST",
-			dataType:"json",
-			url: "php/utilerias.php",
-			data:parametros,
-			succes: function(response){
-				if (response.respuesta == true)
-				{
-					$("·entradaUsuario").hide();
-					$("nav").show();
+		var parametros = "opcion=valida"+
+		                 "&usuario="+usuario+
+		                 "&clave="+clave+
+		                 "&id="+Math.random();
+		
+		//Validamos que no esten vacíos
+		if(usuario!="" && clave!="")
+		{
+			//Hacemos la petición remota
+			$.ajax({
+				cache:false,
+				type:"POST",
+				dataType:"json",
+				url: "php/utilerias.php",
+				data:parametros,
+				success: function(response){
+					if(response.respuesta == true)
+					{    
+						$("#entradaUsuario").hide("slow");
+						$("nav").show("slow"); 
+					}
+					else
+					{
+						alert("Datos incorrectos :(");
+					}
+				},
+				error: function(xhr,ajaxOptions,thrownError){
+					//Si todo sale mal
 				}
+			});
+		}
+		else
+		{
+			alert("Usuario y clave son obligatorios");
+		}
+	}
+	$("#btnValidaUsuario").on("click",validaUsuario);
+}
+//Evento inicial
+$(document).on("ready",inicioUsuarios);
 
-				else{
-					alert("Datos incorrectoss :(");
-				}
-//Si todo sale bien
-			},
-			error:function(xhr,ajaxOptions,thrownError){
-//Si todo sale mal
-			}
 
-
-		});
 
 		//validamos
-		if (usuario!="" && clave!="")
+		/*if (usuario!="" && clave!="")
 		{
 
 		}
@@ -44,13 +63,21 @@ var inicioUsuarios = function()
 			alert ("Usuario y clave son obligatorios");
 		}
 	}
-
-
-
-
-	$("#btnValidaUsuario").on("click",validaUsuario);
+		$("btnValidaUsuario").on("click",validaUsuario);
+var teclaClave = function(tecla)
+{
+	if(tecla.which == )//tecla enter.
+	{
+		validaUsuario(); //Función que valida al usuario.
+	}
 
 }
+	//keypress:_ se ejecuta cada vez que presiono una
+	//tecla sobre el input.
+	$("txtClave").on("keypress" ,teclaClave);
 
+
+}
+*/
 //Evento inicial
-$(document).on("ready,inicioUsuarios");
+
--- usuarios/php/utilerias.php
+++ usuarios/php/utilerias.php
@@ -1,62 +1,57 @@
 <?php
-	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
+  //Limpiar parámetros - contra ataques
+  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+  {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+    $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+    switch ($theType) {
+      case "text":
+        $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+        break;    
+      case "long":
+      case "int":
+        $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+        break;
+      case "double":
+        $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+        break;
+      case "date":
+        $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+        break;
+      case "defined":
+        $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+        break;
+    }
+    return $theValue;
   }
-  return $theValue;
-}
-
-	function validaUsuario()
-	{
-    $respuesta = false;
-		$u = GetSQLValueString ($_POST["usuario"],"text");//limpieza
-		$c = GetSQLValueString ($_POST["clave"], "text");//limpieza
-		$conexion=mysql_connect("localhost","root","");
-    //conexion a la base de datos
+  function validaUsuario()
+  {   
+    $respuesta = false;     
+    $u = GetSQLValueString($_POST["usuario"],"text"); //limpieza
+    $c = GetSQLValueString($_POST["clave"],"text"); //limpieza
+    //Conexión al servidor
+    $conexion  = mysql_connect("localhost","root","");
+    //Conexión a la base de datos
     mysql_select_db("bd2163");
-		$consulta = sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s limit 1",$u,$c);
+    $consulta  = sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s limit 1",$u,$c);
     $resultado = mysql_query($consulta);
     //Esperamos un solo resultado
-    if (mysql_num_rows($resultado) ==1)
+    if(mysql_num_rows($resultado) == 1)
     {
-        $respuesta = true;
-
+      $respuesta = true;
     }
-    $arregloJSON = array('resouesta' => , $respuesta);
+    $arregloJSON = array('respuesta' => $respuesta );
     print json_encode($arregloJSON);
-
-	}
-
-  //menù principal
-	$opc=$_POST["opcion"];
-	swtich($opc){
-		dcase 'valida'
-		validaUsuario();
-		break;
-
-		default:
-		# code...
-		break;
-
-	}
-	?>
+  }
+  //Menú principal
+  $opc = $_POST["opcion"];
+  switch ($opc) {
+    case 'valida':
+      validaUsuario();
+      break;
+    
+    default:
+      # code...
+      break;
+  }
+?>
